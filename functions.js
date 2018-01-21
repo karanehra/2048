31448f51e0d048a8aa2cf69047da2be29b93fae3
Karan Nehra <karanehra@gmail.com>
2018-01-22 03:35:18 +0530

    First commit

---
+++ functions.js
@@ -0,0 +1,29 @@
+function initialize(){
+	var value_table = [
+		[0,0,0,0,0],
+		[0,0,0,0,0],
+		[0,0,0,0,0],
+		[0,0,0,0,0],
+		[0,0,0,0,0]
+	]
+
+	value_table = randomizer(value_table);
+	
+}
+
+function randomizer(object){
+	var free_spaces = [];
+	var table = object;
+	for (var i = 0; i <5; i++) {
+		for (var j = 0; j < 5; j++) {
+			if (table[i][j] == 0) {
+				free_spaces.push([i,j])
+			}
+		}
+	}
+	var len = free_spaces.length
+	var rand =Math.floor(Math.random()*(len-1))
+	var rand_coord = free_spaces[rand];
+	table[rand_coord[0]][rand_coord[1]] = 2
+	return table
+}
